fc1df1b94b8c4687a31e6b2c6b3d834be4624e6d
Saransh Chauhan <k.saranshchauhan@gmail.com>
2024-10-30 00:15:34 +0530

    created book checkout and related api's

---
+++ src/main/java/com/saransh/spring_boot_library/config/SecurityConfiguration.java
@@ -0,0 +1,5 @@
+package com.saransh.spring_boot_library.config;
+
+public class SecurityConfiguration {
+    
+}
---
+++ src/main/java/com/saransh/spring_boot_library/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.saransh.spring_boot_library.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saransh.spring_boot_library.entity.Book;
+import com.saransh.spring_boot_library.service.BookService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    
+    private BookService bookService;
+    // @Autowired
+    public BookController(BookService bookService){
+        this.bookService=bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception{
+        String userEmail="test@gmail.com";
+        return bookService.checkoutBook(userEmail,bookId);
+
+    }
+    @GetMapping("/secure/ischeckedoutbyuser")
+    public Boolean checkedOutBookByUser(@RequestParam Long bookId) {
+        String userEmail="test@gmail.com";
+        return bookService.checkoutBookByUser(userEmail, bookId);
+    }
+    @GetMapping("/secure/currentloans/count")
+    public int currentLoansCount() {
+        String userEmail="test@gmail.com";
+        return bookService.currentLoansCount(userEmail);
+    }
+    
+    
+}
+
